e1b888264da8d10de90ae91ae0f79b6c2c0c7fe6
munim <muhammadmunimoff330@gmail.com>
2024-07-20 01:08:15 +0500

    nav and footer added

---
+++ src/app/Components/Footer.jsx
@@ -0,0 +1,168 @@
+// 'use client';
+
+// import Image from 'next/image';
+// import Link from 'next/link';
+// import { motion, AnimatePresence } from "framer-motion";
+
+// import { FaGithub } from "react-icons/fa";
+// import { IoLogoLinkedin } from "react-icons/io5";
+// import { FaInstagram } from "react-icons/fa6";
+// import { FaSquareXTwitter } from "react-icons/fa6";
+// import { HiOutlineMail } from "react-icons/hi";
+// import { FaSquareWhatsapp } from "react-icons/fa6";
+
+// const Footer = () => {
+//   const currentDate = new Date();
+
+//   // Extract the current year
+//   const currentYear = currentDate.getFullYear();
+
+//   const linkVariants = {
+//     hover: {
+//       scale: 1.1,
+//       transition: {
+//         type: "spring",
+//         stiffness: 300,
+//         damping: 20,
+//       },
+//     },
+//   };
+
+//   return (
+//     <footer className='bg-blue-600 h-48 text-white'>
+//       <div className="flex flex-col items-center px-8">
+//         <div className="top bg-red-700 h-40 w-full flex items-center justify-between">
+//           <div className="logo w-2/6 flex items-center justify-center">
+//             <Image src="Logo.svg" alt="Logo" width={116} height={48} priority />
+//           </div>
+
+//           <div className="links capitalize w-2/6 flex flex-col items-center justify-center">
+//             {['/', '/about', '/work', , '/hire-me', '/projects', '/testimonials'].map((href, index) => (
+//               <motion.div key={index} variants={linkVariants} whileHover="hover">
+//                 <Link href={href}>{href.substring(1) || 'Home'}</Link>
+//               </motion.div>
+//             ))}
+//           </div>
+
+//           <div className="w-2/6 flex items-center justify-center flex-col gap-3">
+//             <address className='block'>Address:<br />Block F, Multi Garden B-17, Islamabad, PK</address>
+
+//             <div className="socialLinks flex items-center justify-center text-2xl gap-2">
+//               <a href="">
+//                 <IoLogoLinkedin />
+//               </a>
+//               <a href="">
+//                 <FaGithub />
+//               </a>
+//               <a href="">
+//                 <FaInstagram />
+//               </a>
+//               <a href="">
+//                 <FaSquareXTwitter />
+//               </a>
+//               <a href="">
+//                 <HiOutlineMail />
+//               </a>
+//               <a href="">
+//                 <FaSquareWhatsapp />
+//               </a>
+//             </div>
+//           </div>
+//         </div>
+//         <div className="bottom bg-red-400 h-8 w-full flex items-center justify-center">
+//           <p className="">Copyright &copy; {currentYear} - Munim</p>
+//         </div>
+//       </div>
+//     </footer>
+//   )
+// }
+
+// export default Footer
+
+
+
+
+
+'use client';
+
+import Image from 'next/image';
+import Link from 'next/link';
+import { motion } from "framer-motion";
+
+import { FaGithub } from "react-icons/fa";
+import { IoLogoLinkedin } from "react-icons/io5";
+import { FaInstagram } from "react-icons/fa6";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { HiOutlineMail } from "react-icons/hi";
+import { FaSquareWhatsapp } from "react-icons/fa6";
+
+const Footer = () => {
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+
+  const linkVariants = {
+    hover: {
+      scale: 1.1,
+      transition: {
+        type: "spring",
+        stiffness: 300,
+        damping: 20,
+      },
+    },
+  };
+
+  const socialLinkVariants = {
+    hover: {
+      scale: 1.3,
+      transition: {
+        type: "spring",
+        stiffness: 300,
+        damping: 20,
+      },
+    },
+  };
+
+  return (
+    <footer className='bg-blue-600 text-white'>
+      <div className="flex flex-col items-center px-8 py-4">
+        <div className="w-full flex flex-col md:flex-row items-center justify-between space-y-4 md:space-y-0">
+          <div className="flex items-center justify-center md:w-1/3">
+            <Image src="/Logo.svg" alt="Logo" width={116} height={48} priority />
+          </div>
+
+          <div className="flex md:flex-col flex-row items-center justify-center gap-4 md:gap-0 md:w-1/3 space-y-1 capitalize">
+            {['/', '/about', '/work', '/hire-me', '/projects', '/testimonials'].map((href, index) => (
+              <motion.div key={index} variants={linkVariants} whileHover="hover">
+                <Link href={href}>{href.substring(1) || 'Home'}</Link>
+              </motion.div>
+            ))}
+          </div>
+
+          <div className="flex flex-col items-center justify-center md:w-1/3 space-y-3">
+            <address className='not-italic text-center'>Address:<br />Block F, Multi Garden B-17, Islamabad, PK</address>
+
+            <div className="flex items-center justify-center text-2xl gap-2">
+              {[
+                { href: "", icon: <IoLogoLinkedin /> },
+                { href: "", icon: <FaGithub /> },
+                { href: "", icon: <FaInstagram /> },
+                { href: "", icon: <FaSquareXTwitter /> },
+                { href: "", icon: <HiOutlineMail /> },
+                { href: "", icon: <FaSquareWhatsapp /> },
+              ].map((social, index) => (
+                <motion.a key={index} href={social.href} variants={socialLinkVariants} whileHover="hover">
+                  {social.icon}
+                </motion.a>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="w-full mt-4 flex items-center justify-center">
+          <p>&copy; {currentYear} - Munim</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/app/Components/Nav.jsx
@@ -0,0 +1,134 @@
+'use client';
+
+import { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { motion, AnimatePresence } from "framer-motion";
+
+const Nav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const menuVariants = {
+    open: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        type: "spring",
+        stiffness: 100,
+        damping: 10,
+      },
+    },
+    closed: {
+      opacity: 0,
+      y: -20,
+      transition: {
+        type: "spring",
+        stiffness: 100,
+        damping: 10,
+      },
+    },
+  };
+
+  const linkVariants = {
+    hover: {
+      scale: 1.1,
+      transition: {
+        type: "spring",
+        stiffness: 300,
+        damping: 20,
+      },
+    },
+  };
+
+  const svgVariants = {
+    open: {
+      rotate: 180,
+      transition: {
+        type: "spring",
+        stiffness: 300,
+        damping: 20,
+      },
+    },
+    closed: {
+      rotate: 0,
+      transition: {
+        type: "spring",
+        stiffness: 300,
+        damping: 20,
+      },
+    },
+  };
+
+  return (
+    <nav className="bg-red-400 w-full">
+      <div className="flex items-center justify-between h-20 px-4">
+        <div className="logo w-1/6 ">
+          <Image src="Logo.svg" alt="Logo" width={116} height={48} priority />
+        </div>
+        <div className="hidden md:flex items-center justify-end gap-5 w-5/6">
+          <div className="links text-white flex items-center gap-5 capitalize">
+            {['/', '/about', '/work', '/projects', '/testimonials', '/hire-me'].map((href, index) => (
+              <motion.div key={index} variants={linkVariants} whileHover="hover">
+                <Link href={href}>{href.substring(1) || 'Home'}</Link>
+              </motion.div>
+            ))}
+          </div>
+          <div className="btn">
+            <button className="btnBlue">
+              <Link href="/contact">Contact</Link>
+            </button>
+          </div>
+        </div>
+        <div className="md:hidden">
+          <motion.button
+            onClick={() => setIsOpen(!isOpen)}
+            className="text-white focus:outline-none"
+            animate={isOpen ? "open" : "closed"}
+            variants={svgVariants}
+          >
+            {isOpen ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
+              </svg>
+            )}
+          </motion.button>
+        </div>
+      </div>
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            initial="closed"
+            animate="open"
+            exit="closed"
+            variants={menuVariants}
+            className="md:hidden flex flex-col items-center bg-red-400 py-2"
+          >
+            {['/', '/about', '/work', '/projects', '/testimonials', '/hire-me', '/contact'].map((href, index) => (
+              <Link key={index} href={href} onClick={() => setIsOpen(false)}>
+                {href.substring(1) || 'Home'}
+              </Link>
+            ))}
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </nav>
+  );
+};
+
+export default Nav;
